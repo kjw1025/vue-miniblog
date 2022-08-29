03526a5ba427cfec349db5d4c4711850cf06de4a
= <=>
2022-08-29 10:00:27 +0900

    전체목록 삭제기능 추가

--- src/components/BlogFooter.vue
+++ src/components/BlogFooter.vue
@@ -1,15 +1,49 @@
 <template>
-    <div>
-        <h1>Footer</h1>
+    <div class="clear-all-wrap">
+        <span class="clear-all-bt" @click="clearAllMemo">CLear All</span>
+        <span class="copy">Copyright 2022 by KJW</span>
     </div>
 </template>
 
 <script>
     export default {
+        setup() {
+            const clearAllMemo = () => {
+                localStorage.clear();
+                
+            }
 
+            return {
+                clearAllMemo
+            }
+        }
     }
 </script>
 
 <style>
-
+    .clear-all-wrap {
+        position: relative;
+        display: block;
+        width: 100%;
+        /* height: 50px; */
+        line-height: 50px;
+        background-color: #fff;
+        text-align: center;
+        margin: 0 auto;
+        border-radius: 5px;
+    }
+    .clear-all-bt {
+        display: inline-block;
+        width: 80%;
+        height: 50px;
+        cursor: pointer;
+        border: 1px solid #7d7d7d;
+        border-radius: 5px;
+        margin: 10px;
+    }
+    .copy {
+        display: block;
+        font-size: 9px;
+        white-space: nowrap;
+    }
 </style>
