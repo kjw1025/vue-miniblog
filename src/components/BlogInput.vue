bc0140ff14e0db1323567988160293a30a7915b7
= <=>
2022-08-29 13:12:53 +0900

    아이콘 및 날짜 추가

--- src/components/BlogInput.vue
+++ src/components/BlogInput.vue
@@ -1,6 +1,15 @@
 <template>
     <div class="input-wrap shadow">
         <input type="text" v-model="newItem" class="input-box" maxlength="30" @keyup.enter="addItem">
+
+        <span @click="addIcon(0)" class="img1">
+            이미지1
+        </span>
+        <span @click="addIcon(1)" class="img2">
+            이미지2
+        </span>
+
+
         <span @click="addItem" class="add-bt">
             <i class="fas fa-plus add-bt-icon"></i>
         </span>
@@ -12,52 +21,32 @@
         ref
     } from 'vue';
     export default {
-        setup() {
+        setup(props, context) {
             const newItem = ref('');
-
-            // 현재 시간값을 계산해서 중복이 되지 않는 값을 처리한다.
-            // 용도는 key 와 id 를 생성해 주기 위해서 처리      
-            // 10보다 작은 값에 0을 붙임
-            const addZero = (n) => {
-                return n < 10 ? '0' + n : n;
-            }
-            // 현재 시간을 리턴
-            const getCurrentDate = () => {
-                let date = new Date();
-                return date.getFullYear().toString() + addZero(date.getMonth() + 1) + addZero(date.getDate()) +
-                    addZero(date.getHours()) + addZero(date.getMinutes()) + addZero(date.getSeconds());
-            }
-
+            const newIcon = ref(0);
             const addItem = () => {
                 let temp = newItem.value;
+                let icon = newIcon.value;
                 // 앞쪽 뒷쪽 공백 제거
                 temp = temp.trim();
                 // 추후 업데이트 예정(정규표현식-문자열체크 문법)
                 //  앞자리공백   공백    뒷자리공백
                 if (temp !== '') {
-                    // json 저장 문자열
-                    ///{completed:false, title:메모내용, icon:파일명 ....}
-                    let memoTemp = {
-                        id: getCurrentDate(),
-                        complete: false,
-                        memotitle: newItem.value
-                    };
-
-                    //추후 실제 DB 연동 예정
-                    localStorage.setItem(memoTemp.id, JSON.stringify(memoTemp));
-
+                    context.emit("additem", temp, icon);
                     resetItem();
                 }
             }
-
             // 내용 재설정
             const resetItem = () => {
                 newItem.value = '';
             }
-
+            const addIcon = (index) => {
+                newIcon.value = index;
+            }
             return {
                 newItem,
-                addItem
+                addItem,
+                addIcon
             }
         }
     }
@@ -89,11 +78,35 @@
         margin-left: 20px;
     }
 
+    .img1 {
+        display: inline-block;
+        width: 50px;
+        height: 50px;
+        font-size: 0;
+        cursor: pointer;
+        background: url('@/assets/images/dog_1.png') no-repeat center;
+        background-size: cover;
+    }
+
+    .img2 {
+        display: inline-block;
+        width: 50px;
+        height: 50px;
+        font-size: 0;
+        cursor: pointer;
+        background: url('@/assets/images/dog_2.png') no-repeat center;
+        background-size: cover;
+    }
+
     .add-bt {
         display: block;
         float: right;
-        background-color: hotpink;
+        background-color: navy;
         cursor: pointer;
+    }
+
+    .add-bt:hover>i {
+        transform: rotate(180deg);
     }
 
     .add-bt-icon {
@@ -101,5 +114,7 @@
         vertical-align: middle;
         color: #fff;
         margin: 0 20px;
+
+        transition: all 0.5s;
     }
 </style>
